6672ad7f05a2691ef36350f8ea9487aae281cefc
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-04-25 11:47:56 +0900

    zoom

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -5,8 +7,11 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const ws = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
