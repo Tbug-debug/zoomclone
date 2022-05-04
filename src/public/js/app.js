dc5f7d1665cd7581d33490c62ce8a3c66704a5d0
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2022-05-04 17:13:07 +0900

    zoom

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -12,6 +12,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCarmars() {
   try {
@@ -91,10 +92,11 @@ camerasSelect.addEventListener("input", handleCameraChnage);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -108,6 +110,22 @@ function handleWelcomeSubmit(event) {
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
-socket.on("welcome", () => {
-  console.log("Someone Joined");
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
+
+socket.on("offer", (offer) => {
+  console.log(offer);
+});
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
--- src/server.js
+++ src/server.js
@@ -1,25 +1,26 @@
 import http from "http";
-import { Server } from "socket.io";
+import SocketIO from "socket.io";
 import express from "express";
-import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-
-const server = http.createServer(app);
-const wsServer = new Server(server);
-
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 wsServer.on("connection", (socket) => {
   socket.on("join_room", (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit("Welcome");
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
   });
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
